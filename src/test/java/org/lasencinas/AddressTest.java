f514cfd8ea2a4817ea575aa3e27e650a78ccfb05
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2019-02-20 17:44:43 +0100

    Casos test iniciales pasades: Cosntructor/Getter/

--- src/test/java/org/lasencinas/AddressTest.java
+++ src/test/java/org/lasencinas/AddressTest.java
@@ -1,7 +1,28 @@
 package org.lasencinas;
 
 import static org.junit.Assert.*;
+
+import org.junit.Before;
 import org.junit.Test;
+import org.lasencinas.address.Address;
 
 public class AddressTest {
+
+    private Address address = null;
+
+    @Before
+    public void __init__() {
+        address = new Address();
+    }
+
+    @Test
+    public void constructorTest() {
+        assertNotNull(address);
+    }
+
+    @Test
+    public void getBalanceTest() {
+        address.setBalance(100);
+        assertEquals(100, address.getBalance(), 0.01);
+    }
 }
