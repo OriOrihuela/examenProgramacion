fae8b6ec8e4b3c66dc9d5ccb4fe569ae4b7a6e42
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2019-02-20 17:50:50 +0100

    generateKeyPairTest passed

--- src/test/java/org/lasencinas/AddressTest.java
+++ src/test/java/org/lasencinas/AddressTest.java
@@ -4,7 +4,9 @@ import static org.junit.Assert.*;
 
 import org.junit.Before;
 import org.junit.Test;
+import java.security.KeyPair;
 import org.lasencinas.address.Address;
+import org.lasencinas.gensig.GenSig;
 
 public class AddressTest {
 
@@ -24,5 +26,12 @@ public class AddressTest {
     public void getBalanceTest() {
         address.setBalance(100);
         assertEquals(100, address.getBalance(), 0.01);
+    }
+
+    @Test
+    public void generateKeyPairTest() {
+        KeyPair pair = GenSig.generateKeyPair();
+        address.setSK(pair.getPrivate());
+        address.setPK(pair.getPublic());
     }
 }
